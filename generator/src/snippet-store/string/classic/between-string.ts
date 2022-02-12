3a43646cca0fabe48483aed788423af151500f9a
Olivier Huin <olih@users.noreply.github.com>
2022-02-12 16:13:14 +0000

    Adds between string

---
+++ generator/src/snippet-store/string/classic/between-string.ts
@@ -0,0 +1,10 @@
+export const betweenString =
+  (start: string, end: string, defaultValue: string) =>
+  (text: string): string => {
+    const startIdx = text.indexOf(start);
+    const endIdx = text.indexOf(end);
+    if (startIdx < 0 || endIdx < 0 || startIdx >= endIdx) {
+      return defaultValue;
+    }
+    return text.slice(startIdx+1, endIdx);
+  };
