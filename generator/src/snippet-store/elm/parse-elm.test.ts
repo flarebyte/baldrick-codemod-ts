5152559635207a01dae6e51702a0553c416de4ab
Olivier Huin <olih@users.noreply.github.com>
2022-05-18 08:19:54 +0100

    Parse multiple elm functions

--- generator/src/snippet-store/elm/parse-elm.test.ts
+++ generator/src/snippet-store/elm/parse-elm.test.ts
@@ -72,17 +72,106 @@ setKey : String -> Model -> Model
 setKey key model =
     { model | key = key }
 
-`
+`;
 
 describe('parse-elm', () => {
   describe('parseElmFunction', () => {
     it('should parse a valid function', () => {
-      const actual = parseElmFunction('setId : Maybe String -> Model -> Model', 'setId id model =' )
-      expect(actual).toMatchInlineSnapshot()
-    })
-  })
+      const actual = parseElmFunction([
+        'setId : Maybe String -> Model -> ReturnedModel',
+        'setId id model =',
+      ]);
+      expect(actual).toMatchInlineSnapshot(`
+        Object {
+          "name": "setId",
+          "params": Array [
+            Array [
+              "id",
+              "Maybe String",
+            ],
+            Array [
+              "model",
+              "Model",
+            ],
+          ],
+          "returned": "ReturnedModel",
+        }
+      `);
+    });
+    it('should parse a one parameter function', () => {
+      const actual = parseElmFunction([
+        'setId : Maybe String -> ReturnedModel',
+        'setId id = ',
+      ]);
+      expect(actual).toMatchInlineSnapshot(`
+        Object {
+          "name": "setId",
+          "params": Array [
+            Array [
+              "id",
+              "Maybe String",
+            ],
+          ],
+          "returned": "ReturnedModel",
+        }
+      `);
+    });
+    it('should parse a higher level function', () => {
+      const actual = parseElmFunction([
+        'setId : Maybe String -> ( Int -> String ) ->ReturnedModel',
+        'setId id formatter = ',
+      ]);
+      expect(actual).toMatchInlineSnapshot(`
+        Object {
+          "name": "setId",
+          "params": Array [
+            Array [
+              "id",
+              "Maybe String",
+            ],
+            Array [
+              "formatter",
+              "( Int -> String )",
+            ],
+          ],
+          "returned": "ReturnedModel",
+        }
+      `);
+    });
+  });
   it('should parse a valid elm code', () => {
     const actual = parseElmFunctions(elmCode);
-    expect(actual).toMatchInlineSnapshot();
+    expect(actual).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "name": "setId",
+          "params": Array [
+            Array [
+              "id",
+              "Maybe String",
+            ],
+            Array [
+              "model",
+              "Model",
+            ],
+          ],
+          "returned": "Model",
+        },
+        Object {
+          "name": "setKey",
+          "params": Array [
+            Array [
+              "key",
+              "String",
+            ],
+            Array [
+              "model",
+              "Model",
+            ],
+          ],
+          "returned": "Model",
+        },
+      ]
+    `);
   });
 });
--- generator/src/snippet-store/elm/parse-elm.ts
+++ generator/src/snippet-store/elm/parse-elm.ts
@@ -3,16 +3,9 @@ interface FunctionInfo {
   params: [string, string][];
   returned: string;
 }
-const checkMethodAspect = (line: string) => {
-  return (
-    line.includes('->') &&
-    line.includes(':') &&
-    !line.includes('\\') &&
-    !line.includes('::')
-  );
-};
 
 const trimStrings = (list: string[]) => list.map((s) => s.trim());
+const chompLast = (text: string): string => text.trim().slice(0, -1).trim();
 
 const replaceArrowWithinParenthesis = (text: string) => {
   let r = '';
@@ -39,40 +32,78 @@ const replaceArrowWithinParenthesis = (text: string) => {
   }
 
   return r;
-}
-
-function parseParams(signature: string) {
-  const noArrow = replaceArrowWithinParenthesis(signature);
-  const params = trimStrings(noArrow.split('->'));
-  return params.map( param => param.replace('@', '->'))
-}
-
-function parseMethodSignature(lines: string[], line: string): FunctionInfo {
-  const [methodName, signature] = line.split(':', 2);
-  const signParams = signature ? parseParams(signature): [];
-  const paramTypes = signParams.slice(0, -1);
-  const paramNames = trimStrings(findParameterNames(methodName)(lines));
-  params = zip(paramNames, paramTypes);
-  return {
-    name: methodName?.trim(),
-    params: params,
-    returned: signParams.slice(-1)[0].strip(),
-  };
-}
-
-const matchMethodName = (methodName: string) => (line: string) =>
-  line.startsWith(methodName) && line.endsWith('=');
-
-function extractParamName(line: string, methodName: string) {
-  return line.replace(methodName, '').replace('=', '').trim().split('');
-}
-
-const findParameterNames = (methodName: string) => (lines: string[]) => {
-  const found = lines.find(matchMethodName(methodName));
-  return found ? extractParamName(found, methodName) : [];
 };
 
-export const parseElmFunction = (_signatureLine: string, _parametersLine: string): FunctionInfo | false=> {
-  return false;
-}
-export const parseElmFunctions = (_content: string): FunctionInfo[] => [];
+const parseParams = (signature: string) => {
+  const noArrow = replaceArrowWithinParenthesis(signature);
+  const params = trimStrings(noArrow.split('->'));
+  return params.map((param) => param.replace('@', '->'));
+};
+
+const findElmFunctionBlocks = (content: string): [string, string][] => {
+  const lines = content.split('\n');
+  let firstLine: string | false = false;
+  let name: string | false = false;
+  const results: [string, string][] = [];
+  for (const line of lines) {
+    if (line.includes(':') && line.includes('->')) {
+      const maybeName = line.split(':')[0]?.trim() || false;
+      name = maybeName && /(\w)+/.test(maybeName) ? maybeName : false;
+      if (name) {
+        firstLine = line;
+      }
+    } else if (
+      name &&
+      firstLine &&
+      line.trim().endsWith('=') &&
+      line.trim().startsWith(name)
+    ) {
+      results.push([firstLine, line]);
+      name = false;
+      firstLine = false;
+    } else {
+      name = false;
+      firstLine = false;
+    }
+  }
+  return results;
+};
+
+export const parseElmFunction = (
+  twoLines: [string, string]
+): FunctionInfo | false => {
+  const [signatureLine, parametersLine] = twoLines;
+  const hasEqual = parametersLine.trim().endsWith('=');
+  const hasColon = signatureLine.includes(':');
+  if (!hasEqual || !hasColon) {
+    return false;
+  }
+  const [name, ...parameters] = trimStrings(
+    chompLast(parametersLine).split(' ')
+  );
+  const [signatureName, signature] = trimStrings(signatureLine.split(':', 2));
+  if (!name || name !== signatureName || !signature || !parameters) {
+    return false;
+  }
+  const signatureParamsAndReturn = parseParams(signature);
+  const returned = signatureParamsAndReturn.slice(-1)[0];
+  const signatureParams = signatureParamsAndReturn.slice(0, -1);
+  if (signatureParams.length !== parameters.length || !returned) {
+    return false;
+  }
+  const params: [string, string][] = parameters.map((param, idx) => [
+    param,
+    signatureParams[idx] || '',
+  ]);
+  return {
+    name,
+    params,
+    returned,
+  };
+};
+
+const isFunctionInfo = (value: unknown): value is FunctionInfo =>
+  typeof value === 'object' && value !== null;
+
+export const parseElmFunctions = (content: string): FunctionInfo[] =>
+  findElmFunctionBlocks(content).map(parseElmFunction).filter(isFunctionInfo);
